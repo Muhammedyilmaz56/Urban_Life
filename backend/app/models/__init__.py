e1c1ff37af804bebe795c339db6b052c517b3edd
Muhammed Yilmaz <muhammedyilmaz5666@gmail.com>
2025-12-20 20:45:31 +0300

    hata duzelltıldı

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,8 +2,10 @@
 from .base import Base
 from .user_model import User, UserRole
 from .category_model import Category
-from .complaint_model import Complaint, ComplaintStatus, Priority
 from .complaint_photo_model import ComplaintPhoto
+from .complaint_resolution_photo_model import ComplaintResolutionPhoto
+from .complaint_model import Complaint, ComplaintStatus, Priority
 from .assignment_model import Assignment, AssignmentStatus
 from .notification_model import Notification, NotificationStatus
 from .worker import Worker
+from .complaint_resolution_photo_model import ComplaintResolutionPhoto
